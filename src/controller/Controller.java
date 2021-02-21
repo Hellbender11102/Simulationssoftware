55b1f06986f9493dff17514f76c3faef06c679d2
Schindler <Laurenz.Schindler@stud.th-luebeck.de>
2021-02-21 15:53:41 +0100

    MVC update

---
+++ src/controller/Controller.java
@@ -0,0 +1,12 @@
+package controller;
+
+import view.View;
+
+public class Controller {
+    private View view;
+
+    public Controller(View view) {
+        this.view = view;
+    }
+
+}
---
+++ src/view/View.java
@@ -0,0 +1,11 @@
+package view;
+
+import javax.swing.*;
+
+public class View extends JFrame {
+    public View() {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+}
