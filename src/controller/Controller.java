569554d1a86003825ec0dd118ffa3014838b3867
Schindler <Laurenz.Schindler@stud.th-luebeck.de>
2021-03-17 11:01:21 +0100

    #3 robot working in threads

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -4,29 +4,40 @@ import model.Position;
 import model.Robot;
 import view.View;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Controller {
     private View view;
-    private List<Robot> robotList = new LinkedList<>();
-    private Map<Robot, Position> globalPosition = new HashMap<>();
+    private Map<Robot, Position> robotsAndPositionOffsets = new HashMap<>();
 
     public Controller(View view) {
         this.view = view;
-        robotList.add(new Robot(0, 1, 2));
+        robotsAndPositionOffsets.put(new Robot(0, 1, 2), new Position(0, 0));
+        robotsAndPositionOffsets.put(new Robot(0, 1, 2), new Position(100, 100, 50));
     }
 
     public void simulationLoop() {
-        for (int i = 0; i < 720; i++) {
-            for (Robot robot : robotList) {
-                robot.drive();
-                System.out.println(robot.toString());
-            }
+        for (Robot robot : robotsAndPositionOffsets.keySet()) {
+            robot.start(1000);
+        }
+        for (int i = 0; i < 1000; i++) {
+            view.setRobots(convertPositionsToGlobal(robotsAndPositionOffsets));
+            view.repaint();
         }
     }
 
+    private LinkedList<Position> convertPositionsToGlobal(Map<Robot, Position> localAndOffset) {
+        LinkedList<Position> globalPositionList = new LinkedList<>();
+        for (Robot robot : localAndOffset.keySet()) {
+            globalPositionList.add(transformation(localAndOffset.get(robot), robot.getLocalPosition()));
+        }
+        return globalPositionList;
+    }
 
+    private Position transformation(Position pGlobal, Position pLocal) {
+        double x = pGlobal.getxCoordinate() + pLocal.getxCoordinate();
+        double y = pGlobal.getyCoordinate() + pLocal.getyCoordinate();
+        double rotation = pGlobal.getRotation() + pLocal.getRotation();
+        return new Position(x, y, rotation);
+    }
 }
--- src/view/View.java
+++ src/view/View.java
@@ -1,16 +1,31 @@
 package view;
 
+import model.Position;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.LinkedList;
+import java.awt.Graphics;
 
 public class View extends JFrame {
-    private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private LinkedList<Position> globalPosition = new LinkedList();
 
     public View() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(screenSize.width - (int) (screenSize.width * 0.02), screenSize.height - (int) (screenSize.height * 0.04));
-        this.setVisible(false);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setSize(screenSize.width - (int) (screenSize.width * 0.0), screenSize.height - (int) (screenSize.height * 0.04));
+        this.setVisible(true);
     }
 
 
+    public void setRobots(LinkedList<Position> globalPosition) {
+        this.globalPosition = globalPosition;
+    }
+
+    public void paint(Graphics g) {
+        for (Position pos : globalPosition) {
+            g.setColor(Color.red);
+            g.fillRect((int) pos.getxCoordinate(), (int) pos.getyCoordinate(), 15, 15);
+        }
+    }
 }
